21dae7bc08a8aae979969982ffb37c730c1387b5
willkuswanto <30658447+willkuswanto@users.noreply.github.com>
2023-04-07 02:06:26 +1000

    added nft image placeholders

--- src/comps/SelectNFTs.svelte
+++ src/comps/SelectNFTs.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
     import { originChain, chains } from "../utils/blockchain";
-    import { Grid, Button, Group } from '@svelteuidev/core'
+    import { Container, Grid, Button, Group, Card, Image, Text } from '@svelteuidev/core'
 
     export let walletAddress = "";
     export let chainId = 0;
     export let selection = false;
     export let disclaimers=[];
+    let focus=false;
 
     function confirmSelection() {
         selection = true;
@@ -17,6 +18,16 @@
         console.log("Selection RESET")
     }
 
+    function onFocus() {
+        focus = true;
+        console.log("NFT in focus")
+    }
+
+    function offFocus() {
+        focus = false;
+        console.log("NFT in focus")
+    }
+
 </script>
 
 <div class="container">
@@ -24,11 +35,32 @@
         Step 3: Burn NFTs
     </h2>
     {#if walletAddress && chainId != 0 && disclaimers.length === 2}
-    <Grid>
-        {#each [1,2,3,4,5,6,7,8,9,10,11,12] as n}
-        <Grid.Col md={6} lg={3}>{n}</Grid.Col>
-        {/each}
-    </Grid>
+    <Container>
+        <Grid>
+            {#each [1,2,3,4,5,6] as n}
+            <Grid.Col span={4}>
+                <Card on:mouseover={onFocus} on:mouseleave={offFocus}>
+                    <Card.Section>
+                        <!-- To change src path to NFT metadata if possible -->
+                        <Image
+                            src='../../nft.png'
+                            fit='contain'
+                            alt='NFT'
+                        />
+                        <Group position='center'>
+                            <Text weight={500}>#{n}</Text>
+                        </Group>
+                    </Card.Section>
+                    {#if focus}
+                        <Button variant='light' color='blue' fullSize>
+                            Migrate
+                        </Button>
+                    {/if}  
+                </Card>
+            </Grid.Col>
+            {/each}
+        </Grid>
+    </Container>
     {/if}
 </div>
 
